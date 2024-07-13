8c5c5eb038fd210f8c37af1dda85bf91e3c60989
Walter-Duchi <waltduchi@gmail.com>
2024-07-12 22:37:03 -0500

    navbar y footer maquitacion finalizada

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Navbar</title>
+    <title>Navbar y Footer</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:400&display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,700&display=swap">
@@ -13,7 +13,8 @@
 <body>
     <nav class="navbar">
         <div class="logo">
-            <img src="ruta_del_logo.png" alt="Logo">
+            <img src="assets/img/logo.png" alt="Logo">
+           
         </div>
         <div class="search-bar">
             <input type="text" placeholder="Busca más de 10000 productos">
@@ -51,5 +52,41 @@
             </a>
         </div>
     </nav>
+    
+    <main>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        
+    </main>
+
+    <footer class="footer">
+        <div class="footer-top">
+            <div class="footer-links">
+                <a href="#">Ubicación</a>
+                <a href="#">Términos y condiciones</a>
+                <a href="#">Política de privacidad</a>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="footer-section">
+                <h4>Pago seguro</h4>
+                <i class="fa-solid fa-pager"></i>
+            </div>
+            <div class="footer-section">
+                <h4>Redes Sociales</h4>
+                <a href="#"><i class="fab fa-facebook"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+                <a href="#"><i class="fa-brands fa-tiktok"></i></a>
+            </div>
+            <div class="footer-section">
+                <h4>Contactos</h4>
+                <a href="#"><i class="fa-brands fa-whatsapp"></i></a>
+                <a href="#"><i class="fab fa-instagram"></i></a>
+            </div>
+            <div class="footer-section">
+                <h4>Aceptamos todas las tarjetas de crédito</h4>
+                <i class="fa-solid fa-credit-card"></i>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>
