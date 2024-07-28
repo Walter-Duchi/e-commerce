5d36acb33a0ed9050f5f03c26ccbe62156555c6c
Walter-Duchi <waltduchi@gmail.com>
2024-07-27 23:19:58 -0500

    agregado formularios

--- index.php
+++ index.php
@@ -1,48 +1,51 @@
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Dashboard</title>
-        <link rel="stylesheet" href="css/navbar-footer.css">
-        <link rel="stylesheet" href="../css/navbar-footer.css">
-        <link rel="stylesheet" href="../css/style.css">
-        <link rel="stylesheet" href="css/styles.css">        
-        <script src="assets/js/scripts.js"></script> <!-- Incluir tu archivo JS -->
-        <style>
-            button.cuenta{
-                border-radius: 0;
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="css/navbar-footer.css">
+    <link rel="stylesheet" href="../css/navbar-footer.css">
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="css/styles.css">        
+    <script src="assets/js/scripts.js"></script> <!-- Incluir tu archivo JS -->
+    <style>
+        button.cuenta{
+            border-radius: 0;
+        }
+    </style>
+</head>
+<body>
+    <?php
+    if (isset($_POST['cuenta'])) {
+        include 'redirects/sign-in-up.php';
+        
+    }elseif(isset($_GET['action'])) {
+        
+            $action = $_GET['action'];
+            if ($action == 'registro_cliente') {
+                include 'forms/Cliente.php';
+            } elseif ($action == 'registro_encargado') {
+                include 'forms/EncargadoInventario.php';
+            } elseif ($action == 'iniciar_sesion') {
+                include 'forms/login.php';
             }
-        </style>
-    </head>
-    <body>
-        <!-- EL NAVBAR SIEMPRE ESTA PERO CAMBIA DEPENDE DEL USUARIO -->
-        <!-- El navbar por defecto es de Cliente No Registrado -->
-        <?php include 'navbars/ClienteNoRegistrado.php'; ?>
-        <!-- php include 'navbars/ClienteRegistrado.php'; se debe -->
-        <!-- php include 'navbars/EncargadoInventarios'; -->
+    }else {
+        include 'navbars/ClienteNoRegistrado.php';
 
-        <!-- EL CONTENIDO DEBE CAMBIAR DE ACUERDO LO QUE SELECCIONE EL USUARIO -->
-        <?php
         if (isset($_GET['categoria'])) {
             $categoria = $_GET['categoria'];
             include('views/productosCategoria.php');
-        }elseif (isset($_POST['cuenta'])) {
-            include 'redirects/sign-in-up.php';
-        } elseif (isset($_POST['carrito'])) {
+        }  elseif (isset($_POST['carrito'])) {
             include 'views/carrito.php';
         } elseif (isset($_POST['verProducto'])) {
-            include 'views/producto.php';  
+            include 'views/producto.php';
         } else {
             include 'dashboards/Cliente-Registrado-NoRegistrado.php';
         }
-        ?>
-
-        <link rel="stylesheet" href="css/navbar-footer.css">
-        <!-- EL FOOTER SIEMPRE DEBE ESTAR -->
-        <?php require 'templates/footer.php'; ?>
-        <!-- LA CONEXION SIEMPRE DEBE ESTAR -->
-        <?php require 'database/connection.php'; ?>
-
-    </body>
+        require 'templates/footer.php';
+    }
+    ?>
+    <?php require 'database/connection.php'; ?>
+</body>
 </html>
--- redirects/sign-in-up.php
+++ redirects/sign-in-up.php
@@ -14,9 +14,9 @@
                 ¿Qué te gustaría hacer?
             </div>
             <div class="redirect-content">
-                <button onclick="window.location.href='registro_cliente.html'">Registrarse como Cliente</button>
-                <button onclick="window.location.href='registro_encargado.html'">Registrarse como Vendedor</button>
-                <button onclick="window.location.href='iniciar_sesion.html'">Iniciar Sesión</button>
+                <button onclick="window.location.href='index.php?action=registro_cliente'">Registrarse como Cliente</button>
+                <button onclick="window.location.href='index.php?action=registro_encargado'">Registrarse como Vendedor</button>
+                <button onclick="window.location.href='index.php?action=iniciar_sesion'">Iniciar Sesión</button>
                 <div class="back-to-home" onclick="window.location.href='index.php'">Regresar al Inicio</div>
             </div>
         </div>
