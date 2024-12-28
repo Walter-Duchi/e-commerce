568bc329e32ceae0e08d20eb9fc91145e15ceac3
Walter-Duchi <waltduchi@gmail.com>
2024-12-28 16:27:08 -0500

    add function sentri

--- index.php
+++ index.php
@@ -12,6 +12,44 @@ session_start();
 </head>
 <body>
     <?php
+    require_once 'vendor/autoload.php';
+
+    \Sentry\init([
+        'dsn' => 'https://f5814d466582d1890db17bc96789b239@o4508542759993344.ingest.us.sentry.io/4508546948857856',
+        'traces_sample_rate' => 1.0,
+        'profiles_sample_rate' => 1.0,
+    ]);
+
+    // Configurar el nivel de reporte de errores
+    error_reporting(E_ALL);
+    ini_set('display_errors', '0'); // No mostrar errores en pantalla
+    ini_set('log_errors', '1');    // Registrar errores en logs
+
+    // Capturar errores no manejados
+    set_error_handler(function ($severity, $message, $file, $line) {
+        if (!(error_reporting() & $severity)) {
+            return false; // Ignorar errores no configurados en error_reporting
+        }
+        \Sentry\captureMessage("Error: $message en $file:$line", \Sentry\Event::LEVEL_ERROR);
+        throw new ErrorException($message, 0, $severity, $file, $line);
+    });
+
+    // Capturar excepciones no manejadas
+    set_exception_handler(function ($exception) {
+        \Sentry\captureException($exception);
+    });
+
+    // Capturar errores fatales
+    register_shutdown_function(function () {
+        $error = error_get_last();
+        if ($error && in_array($error['type'], [E_ERROR, E_CORE_ERROR, E_COMPILE_ERROR, E_PARSE])) {
+            \Sentry\captureMessage(
+                "Error fatal: {$error['message']} en {$error['file']}:{$error['line']}",
+                \Sentry\Event::LEVEL_FATAL
+            );
+        }
+    });
+
     require 'database/connection.php';
 
     if (isset($_POST['cuenta'])) {
